bde38725da747fe999e7d9d7cfd72f5c4ad5e8d9
xiejiqing <xiejiqing@163.com>
2019-07-12 23:30:47 +0800

    添加Master:master

---
+++ src/main/java/Master.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2019 KYE Company
+ * 跨越速运集团有限公司版权所有
+ *
+ * @author xiejiqing
+ * @create 2019/7/12 23:29
+ */
+public class Master {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
