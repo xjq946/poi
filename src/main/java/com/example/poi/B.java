2f754052a70fdc0c5b19d6544f998f2c7c109e00
xiejiqing <xiejiqing@163.com>
2019-08-07 18:40:55 +0800

    story#7883
    添加空格

--- src/main/java/com/example/poi/B.java
+++ src/main/java/com/example/poi/B.java
@@ -1,5 +1,8 @@
 package com.example.poi;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Copyright (c) 2019 KYE Company
  * 跨越速运集团有限公司版权所有
@@ -8,7 +11,12 @@ package com.example.poi;
  * @create 2019/7/14 0:12
  */
 public class B {
-    public static void main(String[] args) {
-        System.out.println("B");
+    public static void main(String args[]) {
+        String str = "";
+        String pattern = "(|\\s{0,})";
+
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(str);
+        System.out.println(m.matches());
     }
 }
