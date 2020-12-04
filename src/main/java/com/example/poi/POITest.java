e07b16ac80773e2dd11c98070e999d3fa9cfe802
xiejiqing <xiejiqing@ky-tech.com.cn>
2020-12-04 14:35:09 +0800

    代码优化

--- src/main/java/com/example/poi/POITest.java
+++ src/main/java/com/example/poi/POITest.java
@@ -1,12 +1,5 @@
 package com.example.poi;
 
-/**
- * Copyright (c) 2019 KYE Company
- * 跨越速运集团有限公司版权所有
- *
- * @author xiejiqing
- * @create 2019/7/12 23:09
- */
 public class POITest {
     public static void main(String[] args) {
         System.out.println("hello");
